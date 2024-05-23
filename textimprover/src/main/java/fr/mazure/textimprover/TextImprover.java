46d13e3507bd7d6c9f1eda11794c420b5c380a46
Laurent <laurent@gilgamesh>
2024-05-23 22:19:25 +0200

    feat: call OpenAI

--- textimprover/src/main/java/fr/mazure/textimprover/TextImprover.java
+++ textimprover/src/main/java/fr/mazure/textimprover/TextImprover.java
@@ -1,13 +1,15 @@
 package fr.mazure.textimprover;
 
-/**
- * Hello world!
- *
- */
-public class TextImprover 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+public class TextImprover {
+
+    public static void main(String[] args) {
+
+        OpenAiChatModel model = OpenAiChatModel.withApiKey("demo");
+
+        String joke = model.generate("What is the meaning of life?");
+
+        System.out.println(joke);
     }
 }
