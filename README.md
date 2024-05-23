6790a4c985de5ee96779d4eb915907b3289c31eb
Laurent <laurent@gilgamesh>
2024-05-23 21:37:54 +0200

    feat: initiate project

---
+++ README.md
@@ -0,0 +1,5 @@
+```bash
+cd TextImprover
+mvn clean package
+java -jar target/textimprover-1.0-SNAPSHOT-jar-with-dependencies.jar
+```
---
+++ textimprover/src/main/java/fr/mazure/textimprover/TextImprover.java
@@ -0,0 +1,13 @@
+package fr.mazure.textimprover;
+
+/**
+ * Hello world!
+ *
+ */
+public class TextImprover 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
