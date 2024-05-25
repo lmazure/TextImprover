e0ba58b33e9948bea5a3eea6badc5f03b7ea01b0
Laurent <laurent@gilgamesh>
2024-05-25 11:01:55 +0200

    feat: parse basic args

--- textimprover/src/main/java/fr/mazure/textimprover/TextImprover.java
+++ textimprover/src/main/java/fr/mazure/textimprover/TextImprover.java
@@ -14,12 +14,46 @@ public class TextImprover {
     }
 
     public static void main(final String[] args) {
+        String sysPrompt = "You are a helpful assistant.";
+        String userPrompt = "Hello, how are you?";
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("--system-prompt-string")) {
+                if ((i + 1 ) >= args.length) {
+                    System.err.println("Missing argument for --system-prompt-string");
+                    System.exit(1);
+                }
+                sysPrompt = args[i + 1];
+                i++;
+            } else if (args[i].equals("--user-prompt-string")) {
+                if ((i + 1 ) >= args.length) {
+                    System.err.println("Missing argument for --user-prompt-string");
+                    displayHelpAndExit(1);
+                }
+                userPrompt = args[i + 1];
+                i++;
+            } else if (args[i].equals("--help")) {
+                displayHelpAndExit(0);
+            } else {
+                System.err.println("Unknown argument: " + args[i]);
+                displayHelpAndExit(1);
+            }
+        }
 
+        final SystemMessage systemPrompt = new SystemMessage(sysPrompt);
+
+        final String answerWithName = perform(systemPrompt, userPrompt);
+        System.out.println(answerWithName);
+    }
+
+    private static void displayHelpAndExit(final int exitCode) {
+        final String executableName = "textimprover.jar";
+        System.err.println("Usage: java -jar " + executableName + " --system-prompt-string <system-prompt-string> --user-prompt-string <user-prompt-string>");
+        System.exit(exitCode);
+    }
+
+    private static String perform(final SystemMessage systemPrompt, final String message) {
         final OpenAiChatModel model = OpenAiChatModel.withApiKey("demo");
         final ChatMemory memory = MessageWindowChatMemory.withMaxMessages(2);
-
-        final SystemMessage systemPrompt = new SystemMessage("You will be provided an English text. You have to translate it into French. You should not add any other text.");
-        final String message = "Your case is typical and you are right to fight for compensation.";
 
         memory.add(systemPrompt);
         final Assistant assistant = AiServices.builder(Assistant.class)
@@ -28,6 +62,6 @@ public class TextImprover {
                                               .build();
 
         final String answerWithName = assistant.chat(message);
-        System.out.println(answerWithName);
+        return answerWithName;
     }
 }
