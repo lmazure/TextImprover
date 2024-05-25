387be60b06645c66957386922f7500d01b16ff63
Laurent <laurent@gilgamesh>
2024-05-25 09:05:40 +0200

    feat: add system prompt

--- textimprover/src/main/java/fr/mazure/textimprover/TextImprover.java
+++ textimprover/src/main/java/fr/mazure/textimprover/TextImprover.java
@@ -1,15 +1,33 @@
 package fr.mazure.textimprover;
 
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.service.AiServices;
 
 public class TextImprover {
 
-    public static void main(String[] args) {
+    interface Assistant {
 
-        OpenAiChatModel model = OpenAiChatModel.withApiKey("demo");
+        String chat(String userMessage);
+    }
 
-        String joke = model.generate("What is the meaning of life?");
+    public static void main(final String[] args) {
 
-        System.out.println(joke);
+        final OpenAiChatModel model = OpenAiChatModel.withApiKey("demo");
+        final ChatMemory memory = MessageWindowChatMemory.withMaxMessages(2);
+
+        final SystemMessage systemPrompt = new SystemMessage("You will be provided an English text. You have to translate it into French. You should not add any other text.");
+        final String message = "Your case is typical and you are right to fight for compensation.";
+
+        memory.add(systemPrompt);
+        final Assistant assistant = AiServices.builder(Assistant.class)
+                                              .chatLanguageModel(model)
+                                              .chatMemory(memory)
+                                              .build();
+
+        final String answerWithName = assistant.chat(message);
+        System.out.println(answerWithName);
     }
 }
