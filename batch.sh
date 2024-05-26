20b61d17363fe7c8de4fca74b4308b8ea2e10664
Laurent <laurent@gilgamesh>
2024-05-26 15:41:59 +0200

    feat: Add batch file

---
+++ batch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+find $1 -iname '*.md' -print | while read file
+do
+  echo $file
+  java -jar textimprover/target/textimprover-1.0-SNAPSHOT-jar-with-dependencies.jar --system-prompt-file sysprompt.txt --user-prompt-file $file > $file.new
+  if [ $? -eq 0 ]
+  then
+    mv $file.new $file
+  else
+    rm $file.new
+  fi
+done
